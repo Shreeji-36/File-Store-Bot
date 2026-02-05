4894c2320e0c21fdc0c00aecdc3d916a1ea9aeaf
Shreeji-36 <trivediss158@gmail.com>
2026-02-05 17:31:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ app = Client(
     "FileStoreBot",
     api_id=38528447,
     api_hash=a0b76b9ff89c3f30adbb2696438c6581,
-    bot_token=BOT_TOKEN,
+    bot_token=8578998808:AAHzol7mfZuHHNMT0g3Hv6u9SjiQXwxifJY
     plugins=dict(root="plugins")
 )
 
